286406ff4b183e8d248bec9537af8512a95073fe
Trevor Allred <tallred@picsauditing.com>
2014-03-15 23:28:41 -0700

    got karma and mocha running

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,20 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-karma');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        karma: {
+            unit: {
+                options: {
+                    frameworks: ['mocha'],
+                    autoWatch: true,
+                    browsers: ['PhantomJS'],
+                    singleRun: false,
+                    files: ['test/**/*.js']
+                }
+            }
+        },
         mochaTest: {
             test: {
                 options: {
---
+++ test/sample.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('Sample', function () {
+    it('should test A', function () {
+//        assert.equal('A', 'A');
+        console.info('asdf');
+    });
+});
